b1f80c2a02daece0f5aa41e3d6458b8559559b28
zhengbowang1984@gmail.com <zhengbowang1984@gmail.com@b5971163-24a7-8777-cded-c28a8ab7637f>
2013-12-25 12:45:05 +0000

    # 3.3.8.1-1.0.0 alpha + Add project files - config, build and other files.

---
+++ trunk/JAutoItX-3.3.8.1/build.bat
@@ -0,0 +1,9 @@
+D:
+cd D:\eclipse_for_java\workspace\JAutoItX-3.3.8.1
+
+set JAVA_HOME=D:\Program Files\Java\jdk1.6.0_26
+set ANT_HOME=D:\apache-ant-1.8.2
+
+set path=%JAVA_HOME%\bin;%ANT_HOME%\bin;%path%
+ant
+pause
